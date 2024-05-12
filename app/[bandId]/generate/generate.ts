ae6527512b096a106de555b909cc4a4c02fac4dd
chestercharles <chestercarmer@icloud.com>
2024-05-12 00:51:58 -0700

    Update prompt

--- app/[bandId]/generate/generate.ts
+++ app/[bandId]/generate/generate.ts
@@ -7,6 +7,15 @@ const openai = new OpenAI({
   apiKey: process.env["OPENAI_API_KEY"], // This is the default and can be omitted
 });
 
+const RESPONSE_JSON_FORMAT = {
+  setlist: [
+    {
+      title: "string",
+      key: "string",
+    },
+  ],
+};
+
 export async function generate(params: { repertoire: Song[]; prompt: string }) {
   const repertoire = params.repertoire.map((song) => {
     return {
@@ -18,10 +27,16 @@ export async function generate(params: { repertoire: Song[]; prompt: string }) {
 
   const systemPrompt = `
   Your purpose is to generate a setlist for a band. 
-  You generally want about 8 songs in a setlist. Each song is usually around 3-4 min long, so adjust the number of songs accordingly if the user provides a length of time for the set.
+  You generally want about 8 songs in a setlist. 
+  Each song is usually around 3-4 min long, so adjust the number of songs accordingly if the user provides a length of time for the set.
   The user will provide you with a prompt, and your job is to use that prompt to generate a setlist given a song repertoine in JSON. 
-  Your response should be an array of objects with the fields "title" and "key". 
-  Here is the song repertoire in JSON format: ${JSON.stringify(repertoire)}
+  
+  Your response should in the following JSON format: 
+  ${JSON.stringify(RESPONSE_JSON_FORMAT)} 
+
+  Here is the song repertoire in JSON format:
+  
+  ${JSON.stringify(repertoire)}
   `;
 
   const chatCompletion = await openai.chat.completions.create({
