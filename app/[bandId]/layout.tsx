813be9e6f4c9d7143f991f127c9e372d2707945e
chestercharles <chestercarmer@icloud.com>
2024-05-12 22:59:21 -0700

    Add dark mode

--- app/[bandId]/layout.tsx
+++ app/[bandId]/layout.tsx
@@ -47,7 +47,7 @@ export default function BandLayout({
     <div>
       <nav>
         <div className="flex flex-wrap items-center justify-between p-8">
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+          <span className="self-center text-2xl font-semibold whitespace-nowrap">
             <Link href={`/${band.id}/settings`}>{band.name}</Link>
           </span>
           <MobileNavbar options={options} />
@@ -89,7 +89,7 @@ function MobileNavbar({ options }: { options: MenuOption[] }) {
         <button
           data-collapse-toggle="navbar-default"
           type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden focus:outline-none focus:ring-2"
           aria-controls="navbar-default"
           aria-expanded="false"
           onClick={() => setOpen((open) => !open)}
@@ -118,7 +118,7 @@ function MobileNavbar({ options }: { options: MenuOption[] }) {
             <Link
               href={option.href}
               onClick={() => setOpen(false)}
-              className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              className="block py-2 px-3 md:border-0"
             >
               {option.name}
             </Link>
@@ -132,12 +132,12 @@ function MobileNavbar({ options }: { options: MenuOption[] }) {
 function FullWidthNavbar({ options }: { options: MenuOption[] }) {
   return (
     <div className="hidden w-full md:block md:w-auto">
-      <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+      <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
         {options.map((option) => (
           <li key={option.name}>
             <Link
               href={option.href}
-              className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              className="block py-2 px-3 rounded md:border-0 md:p-0"
             >
               {option.name}
             </Link>
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthGuard } from "./AuthGuard";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,9 +29,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <AuthGuard>{children}</AuthGuard>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <AuthGuard>{children}</AuthGuard>
+        </ThemeProvider>
       </body>
     </html>
   );
