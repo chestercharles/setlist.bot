0256849b0f5d9edf88d069277a54df234a5e5d09
chestercharles <chestercarmer@icloud.com>
2024-05-13 22:09:23 -0700

    Bump max duration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  functions: {
+    "src/app/**/*": {
+      maxDuration: 300,
+    },
+  },
+};
 
 export default nextConfig;
